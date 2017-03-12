93dcc7fe79c2ebb04b147f48f50ccfa46a17a2b8
NetoDevelm <josevieira.dev@gmail.com>
2017-03-12 19:01:40 -0300

    match url regex

---
+++ src/main/java/br/com/rileyframework/controller/UserController.java
@@ -0,0 +1,27 @@
+package br.com.rileyframework.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.rileyframework.annotations.Get;
+import br.com.rileyframework.annotations.Rest;
+import br.com.rileyframework.model.User;
+import br.com.rileyframework.serialization.JsonReturn;
+
+@Rest
+public class UserController {
+
+    @Get("/users")
+    public String index() {
+        List<User> users = new ArrayList<User>();
+        users.add(new User("1", "josevieira.dev@gmail.com", "password"));
+        users.add(new User("2", "netodevel@gmail.com", "password"));
+        return JsonReturn.toJson(users);
+    }
+    
+    @Get("/user/{id}/books/{id}")
+    public String testando() {
+    	return "testando";
+    }
+
+}
