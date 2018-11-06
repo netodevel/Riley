35c8b47a2b1f5b85b8881048728ea1f1b79b9758
José Vieira <josevieira.dev@gmail.com>
2018-11-05 22:27:58 -0200

    Adding regex to hvars

--- src/main/java/br/com/rileyframework/ui/RileyTemplateEngine.java
+++ src/main/java/br/com/rileyframework/ui/RileyTemplateEngine.java
@@ -1,9 +1,14 @@
 package br.com.rileyframework.ui;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RileyTemplateEngine {
 
+    private static final String REGEX_PATTERN_HVAR = ("(\\{(\\w+)\\})+");
     private HashMap<String, Object> viewAttributes;
 
     public String format(String row) {
@@ -17,4 +22,14 @@ public class RileyTemplateEngine {
         return this;
     }
 
+    public List<String> hVars(String html) {
+        List<String> hVarsResult = new ArrayList<>();
+        Pattern pattern = Pattern.compile(REGEX_PATTERN_HVAR);
+        Matcher matcher = pattern.matcher(html);
+
+        while (matcher.find()) {
+            hVarsResult.add(matcher.group());
+        }
+        return hVarsResult;
+    }
 }
--- src/test/java/br/com/rileyframework/ui/RileyTemplateEngineTest.java
+++ src/test/java/br/com/rileyframework/ui/RileyTemplateEngineTest.java
@@ -3,6 +3,7 @@ package br.com.rileyframework.ui;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,6 +21,14 @@ public class RileyTemplateEngineTest {
                 .format(hvar);
 
         assertEquals("Hello, Neto", htmlResult);
+    }
+
+
+    @Test
+    public void dado_um_html_deve_retornar_todas_hvar() {
+        String html = " <h1>Hello {world} </h1>";
+        List<String> hVars = new RileyTemplateEngine().hVars(html);
+        assertEquals(1, hVars.size());
     }
 
 }
