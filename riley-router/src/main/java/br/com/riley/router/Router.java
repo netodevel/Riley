25a75704c5a480d961366ed2c3d87aca21774274
Da Silva Neto <josevieira.dev@gmail.com>
2018-11-25 21:48:09 -0200

    Adicionando mais testes de rotas reativas

--- riley-router/src/main/java/br/com/riley/router/Router.java
+++ riley-router/src/main/java/br/com/riley/router/Router.java
@@ -1,9 +1,20 @@
 package br.com.riley.router;
 
+import io.reactivex.Observable;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Router {
 
     protected List<Route> routes = new ArrayList<>();
+
+    public Observable<?> executeRequest(String url) {
+        Optional<Route> routeReturned = routes.stream()
+                .filter(route -> route.getPath().equals(url))
+                .findFirst();
+        if (routeReturned.isPresent()) return routeReturned.get().getReactiveRouteAction().execute();
+        return Observable.just(new RouterException());
+    }
 }
--- riley-router/src/test/java/br/com/riley/router/ReactiveRouterTest.java
+++ riley-router/src/test/java/br/com/riley/router/ReactiveRouterTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static com.greghaskins.spectrum.Spectrum.describe;
 import static com.greghaskins.spectrum.Spectrum.it;
+import static com.greghaskins.spectrum.dsl.specification.Specification.context;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
@@ -28,6 +29,42 @@ public class ReactiveRouterTest {{
                     .execute()
                     .subscribe(res -> { assertEquals("hello world", res); });
         });
+    });
+
+    describe("dado uma url", () -> {
+        String url = "/users";
+        context("com duas rotas registradas", () -> {
+            Route routeIndex = Route.builder().method(HttpConsts.METHOD_GET)
+                    .path("/index").reactiveRouteAction(() -> Observable.just("hello world"))
+                    .build();
+
+            Route routeUsers = Route.builder().method(HttpConsts.METHOD_GET)
+                    .path("/users").reactiveRouteAction(() -> Observable.just("hello users"))
+                    .build();
+
+            List<Route> routes = asList(routeIndex, routeUsers);
+
+            it("deve executar a rota '/users'", () -> {
+                Router router = new Router();
+                router.routes = routes;
+
+                router.executeRequest(url).subscribe(res -> {
+                    assertEquals("hello users", res);
+                });
+            });
+        });
+
+        context("nao encontrada", ()-> {
+            it("deve retornar RouterException", () -> {
+                Router router = new Router();
+                router.routes = asList();
+
+                router.executeRequest(url).subscribe(res -> {
+                    assertEquals(new RouterException(), res);
+                });
+            });
+        });
+
     });
 
 }}
