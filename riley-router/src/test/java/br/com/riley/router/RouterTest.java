8abd2b3458601bada2f1434a1af8e7b5921546fe
Da Silva Neto <josevieira.dev@gmail.com>
2018-11-25 19:29:35 -0200

    Adiciona spectrum bdd style

--- riley-router/src/test/java/br/com/riley/router/RouterTest.java
+++ riley-router/src/test/java/br/com/riley/router/RouterTest.java
@@ -1,23 +1,40 @@
 package br.com.riley.router;
 
+import com.greghaskins.spectrum.Spectrum;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.greghaskins.spectrum.Spectrum.afterEach;
+import static com.greghaskins.spectrum.Spectrum.describe;
+import static com.greghaskins.spectrum.Spectrum.it;
 import static org.junit.Assert.assertEquals;
 
-public class RouterTest {
+@RunWith(Spectrum.class)
+public class RouterTest {{
+    describe("Dado uma lista de rotas", () -> {
+        List<Route> routes = new ArrayList<>();
+        afterEach(routes::clear);
 
-    @Test
-    public void dadoUmaRota_quandoCadastrada_deveRetonarListaDeRotas() {
-        Route route = Route.builder()
-                .method(HttpConsts.METHOD_GET)
-                .path("/home")
-                .routeAction(() -> "action executed")
-                .build();
+        it("deve ser vazia por padrao", () -> {
+            assertEquals(0, routes.size());
+        });
 
-        Router router = new Router();
-        router.routes.add(route);
+        it("deve cadastrar rotas", () -> {
+            Route route = Route.builder()
+                    .method(HttpConsts.METHOD_GET)
+                    .path("/home")
+                    .routeAction(() -> "action executed")
+                    .build();
 
-        assertEquals(1, router.routes.size());
-    }
+            Router router = new Router();
+            router.routes.add(route);
 
-}
+            assertEquals(1, router.routes.size());
+        });
+    });
+}}
+
+
